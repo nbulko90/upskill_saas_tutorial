4255b3fc25affadd17aae552b0a5cd7e2ed7fc21
Nick Bulko <nbulko90@gmail.com>
2017-02-09 16:39:49 +0000

    Added Hirb gem for data presentation

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,19 +1,19 @@
 class ContactsController < ApplicationController
-    def new
-        @contact = Contact.new
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+    redirect_to new_contact_path, notice: "Message Sent!"
+    else
+    redirect_to new_contact_path, notice: "An Error Has Occurred"
     end
-    
-    def create
-        @contact = Contact.new(contact_params)
-        if @contact.save
-            redirect_to new_contact_path, notice: "Message Sent!"
-        else
-            redirect_to new_contact_path, notice: "An Error Has Occurred"
-        end
-    end
-        
-    private
+  end
+
+  private
     def contact_params
-        params.require(:contact).permit(:name, :email, :comments)
+      params.require(:contact).permit(:name, :email, :comments)
     end
-end
+  end

1bb177881cdef26373de15ddc4d35c6155b75c3c
Nick Bulko <nbulko90@gmail.com>
2017-02-25 15:07:07 +0000

    Added profile update action and update button to view profile page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -25,6 +25,22 @@ class ProfilesController < ApplicationController
     @user = User.find( params[:user_id] )
     @profile = @user.profile
   end
+
+  # PUT to /users/:user_id/profile  
+  def update
+    # Retrieve the user from the database
+    @user = User.find( params[:user_id] )
+    # Retriever that user's profile
+    @profile = @user.profile
+    # Mass assign edited profile attributes and save
+    if @profile.update_attributes(profile_params)
+      flash[:success] = "Profile updated!"
+      # Redirect user to their profile page
+      redirect_to user_path(current_user.id)
+    else
+      render action: edit
+    end
+  end
   
   private
     def profile_params
